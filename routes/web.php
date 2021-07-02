c16a263fcbe188cadb1e124fe19538fcbd518a1c
Christopher Baptista <christopherbaptista.19051@mhs.its.ac.id>
2021-07-02 10:40:34 +0700

    jetstream project

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    
+    Route::get('/dashboard', '\App\Http\Controllers\DashboardController@index')->name('dashboard');
+
+    Route::get('products/{id}/gallery', '\App\Http\Controllers\ProductController@gallery')
+        ->name('products.gallery');
+    Route::resource('products', '\App\Http\Controllers\ProductController');
+
+    Route::resource('product-galleries', '\App\Http\Controllers\ProductGalleryController');
+
+    Route::get('transactions/{id}/set-status', '\App\Http\Controllers\TransactionController@setStatus')
+        ->name('transactions.status');
+    Route::resource('transactions', '\App\Http\Controllers\TransactionController');
+});
