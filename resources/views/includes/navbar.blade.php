bf4349bf1c8fc8ec78514208b3914e74aa19498e
Christopher Baptista <57831206+christopherbaptista@users.noreply.github.com>
2021-07-03 23:09:44 +0700

    Update navbar.blade.php

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -2,7 +2,7 @@
 <header id="header" class="header">
     <div class="top-left">
         <div class="navbar-header">
-            <a class="navbar-brand" href="./"><img src="{{ url('images/logo-pangkasnesia.png') }}" alt="Logo"></a>
+            <a class="navbar-brand" href="http://localhost:8081/#/Home"><img src="{{ url('images/logo-pangkasnesia.png') }}" alt="Logo"></a>
             <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
         </div>
     </div>
